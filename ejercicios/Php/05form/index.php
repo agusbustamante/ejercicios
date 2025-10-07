92ac1b35fbd124a7ed9113c6865beb36661ce82c
agusbustamante <168001990+agusbustamante@users.noreply.github.com>
2025-10-06 23:16:44 -0300

    Add files via upload

---
+++ ejercicios/Php/05form/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulario</title>
+</head>
+<body>
+
+<form action="respuesta.php" method="get">
+    Nombre y apellido:<br>
+    <input type="text" name="nombre"><br><br>
+    Edad:<br>
+    <input type="text" name="edad"><br><br>
+    <input type="submit" value="Ingrese la informacion">
+</form>
+
+</body>
+</html>
