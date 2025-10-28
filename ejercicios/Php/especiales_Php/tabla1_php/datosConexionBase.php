b93443149ecb6f22bd45c72c91d38240981d21d3
agusbustamante <168001990+agusbustamante@users.noreply.github.com>
2025-10-28 14:33:25 -0300

    Update datosConexionBase.php

--- ejercicios/Php/especiales_Php/tabla1_php/datosConexionBase.php
+++ ejercicios/Php/especiales_Php/tabla1_php/datosConexionBase.php
@@ -1,11 +1,14 @@
 <?php
-$host     = '127.0.0.1';
-$port     = '4040';
-$dbname   = 'liquidacionesdesueldos';
-$user     = 'root';
-$password = '';
+
+$host     = '127.0.0.1';           
+$port     = '';                    
+$dbname   = 'liquidacionesdesueldos'; 
+$user     = 'root';                
+$password = '';                    
 $charset  = 'utf8mb4';
-$localCfg = __DIR__ . '/db_config.local.php';
-if (file_exists($localCfg)) {
-	include $localCfg; 
+
+
+$local = __DIR__ . '/db_config.local.php';
+if (file_exists($local)) {
+    require $local;
 }
